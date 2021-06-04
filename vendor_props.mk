c17865fb1a98e79130279b340efe98e384160ff1
viralbanda <viralbandachannel@gmail.com>
2021-06-04 13:05:23 +0000

    RMX1851: Improve scrolling, ui smoothness and Ram management

--- vendor_props.mk
+++ vendor_props.mk
@@ -161,3 +161,22 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.zram.mark_idle_delay_mins=60 \
     ro.zram.first_wb_delay_mins=1440 \
     ro.zram.periodic_wb_delay_hours=24
+
+# Additional props
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.cgroup_follow.enable=true
+    persist.vendor.qti.inputopts.enable=true
+    persist.vendor.qti.inputopts.movetouchslop=0.6
+    ro.qcom.adreno.qgl.ShaderStorageImageExtendedFormats=0
+
+# Better Ram
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.fha_enable=true \
+    ro.sys.fw.bg_apps_limit=32 \
+    ro.config.dha_cached_max=16 \
+    ro.config.dha_empty_max=42 \
+    ro.config.dha_empty_init=32 \
+    ro.config.dha_lmk_scale=0.545 \
+    ro.config.dha_th_rate=2.3 \
+    ro.config.sdha_apps_bg_max=64 \
+    ro.config.sdha_apps_bg_min=8
