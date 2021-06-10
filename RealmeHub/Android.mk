e536050aabbc6fa11256346efac6f9ade2b3a8f6
viralbanda <viralbandachannel@gmail.com>
2021-06-10 16:16:56 +0000

    KharaMeParts: Rebrand to RealmeHub

--- RealmeHub/Android.mk
+++ RealmeHub/Android.mk
@@ -28,7 +28,7 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.recyclerview_recyclerview
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_PACKAGE_NAME := KharaMeParts
+LOCAL_PACKAGE_NAME := RealmeHub
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
